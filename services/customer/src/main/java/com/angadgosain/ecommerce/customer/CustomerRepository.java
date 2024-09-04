ea3225de4d81fca98c73cece5fd28ac4177ab452
Angad Anil Gosain <angadgosain@gmail.com>
2024-09-04 16:09:46 -0700

    Completed Building Customer microservice

---
+++ services/customer/src/main/java/com/angadgosain/ecommerce/customer/CustomerRepository.java
@@ -0,0 +1,6 @@
+package com.angadgosain.ecommerce.customer;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CustomerRepository  extends MongoRepository<Customer, String>{
+}
