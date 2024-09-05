8cb032eedabb336ec2d9bb56a70bc95f34e1779f
Angad Anil Gosain <angadgosain@gmail.com>
2024-09-04 20:06:37 -0700

    Initialised database for Product Microservice

---
+++ services/product/src/main/java/com/angadgosain/ecommerce/product/Product.java
@@ -0,0 +1,30 @@
+package com.angadgosain.ecommerce.product;
+
+
+import com.angadgosain.ecommerce.category.Category;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_seq")
+    @SequenceGenerator(name = "product_seq", sequenceName = "product_seq", allocationSize = 50)
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name="category_id")
+    private Category category;
+
+}
