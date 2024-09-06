7f0cecdf402d6b9b1e2a4db713f8a09f8ddea821
Angad Anil Gosain <angadgosain@gmail.com>
2024-09-05 19:21:25 -0700

    Started implementation of Order service.

---
+++ services/order/src/main/java/com/angadgosain/ecommerce/order/OrderService.java
@@ -0,0 +1,12 @@
+package com.angadgosain.ecommerce.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    public Integer createOrder(OrderRequest request) {
+        return null;
+    }
+}
